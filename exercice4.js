a882062636bf56fab71e4c5744424fd8955e4f2e
overom <36300966+overom@users.noreply.github.com>
2021-01-15 19:55:07 +0100

    result

--- exercice4.js
+++ exercice4.js
@@ -3,9 +3,13 @@
 // For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
 
 function validSpacing(s) {
-  // write your code here
-}
+  if (s === "") return true;
+  const a = s.split(" ").filter((elt) => elt === "");
 
+  if (a.length >= 1) return false;
+  else return true;
+}
+validSpacing("Hello world   ");
 // 'Hello world' = true
 // ' Hello world' = false
 // 'Hello world  ' = false
@@ -17,3 +21,16 @@ function validSpacing(s) {
 // 'Helloworld ' = false
 // ' ' = false
 // '' = true
+
+function validSpacing(s) {
+  return s.trim() == s && !s.includes("  ");
+}
+
+function validSpacing(s) {
+  if (s == "") return true;
+  else return !s.split(" ").includes("");
+}
+
+function validSpacing(s) {
+  return !/^[\s]|([\s][\W])|[\s]$/g.test(s);
+}
