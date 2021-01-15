4ae47483c879e23c2525cd945fe7eba0e8110b46
overom <36300966+overom@users.noreply.github.com>
2021-01-15 12:46:26 +0100

    exercice response 1

--- exercice.js
+++ exercice.js
@@ -12,7 +12,25 @@
 //     Young under 21 old.
 //     Adults have 21 or more.
 
-function peopleWithAgeDrink(old) {}
+const peopleWithAgeDrink = (old) => {
+  if (old < 0) {
+    return "Error";
+  } else if (old < 14) {
+    return "drink toddy";
+  } else if (old < 18) {
+    return "drink coke";
+  } else if (old < 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+};
+
+const result = peopleWithAgeDrink(5);
+
+console.log("====================================");
+console.log(result);
+console.log("====================================");
 
 // Exemple :
 // peopleWithAgeDrink(13) === "drink toddy";
